99ac9f4fc0f4ff362a164e135bcc858c1f022b8c
zhao-mm <zhaoyc19951217@126.com>
2019-12-30 14:46:21 +0800

    login status

--- src/main/java/com/zyc/mawen/controller/AuthorizeController.java
+++ src/main/java/com/zyc/mawen/controller/AuthorizeController.java
@@ -4,30 +4,48 @@ import com.zyc.mawen.dto.AccessTokenDTO;
 import com.zyc.mawen.dto.GithubUser;
 import com.zyc.mawen.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                                   HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8087/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("f04ebec2fb1666a2f02c");
-        accessTokenDTO.setClient_secret("842c8e9debce62b4726f5574f047554324db5af8");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        return "index";
+        //System.out.println(user.getName());
+        //System.out.println(user.getId());
+        if(user!=null){
+            //登录成功
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else{
+            //登录时便
+            return "redirect:/";
+        }
+
     }
 
 }
